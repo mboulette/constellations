18303a763b1198811c51636eed11c9ee9bfd9ee2
mboulette <mboulette@idealtechnology.net>
2017-05-01 13:11:25 -0400

    ajouter les astéroids et le loader

--- js/planets.js
+++ js/planets.js
@@ -1,64 +1,230 @@
-planets = [
-
-
-	{
-		"code" : "XCV-21",
-		"name" : "Rh-Paradize",
-		"size" : 5,
-		"rhodium" : 5,
-		"hazard" : 4,
-		"position" : {"x" : 0, "y" : 0, "z" : 0},
-		"description" : "Cette planète affiche un caractère apocalyptique, des rochers tranchants et des ruisseaux de métaux en fusion parsemant sa surface. L’eau est extrêmement rare, et l’aire pratiquement irrespirable. Mais, son potentiel minier est exceptionnel, la quantité de rhodium disponible fait rêver les corporations.",
-		"texture" : "planet-textures/plutomap1k.jpg"
-	},
-
-
-	{
-		"code" : "RJX-32",
-		"name" : "Eugenia",
-		"size" : 1,
-		"rhodium" : 2,
-		"hazard" : 4,
-		"position" : {"x" : 6, "y" : 8, "z" : 4},
-		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta lectus nec velit elementum, et volutpat nibh tempus. Sed bibendum, ipsum eu scelerisque ullamcorper.",
-		"texture" : "planet-textures/Savannah.jpg"
-	},
-
-
-	{
-		"code" : "EOL-7",
-		"name" : "Apus-Avem-VI",
-		"size" : 4,
-		"rhodium" : 1,
-		"hazard" : 1,
-		"position" : {"x" : 11, "y" : 3, "z" : 12},
-		"description" : "Planète plutôt désertique parsemé de certain point d’eau où la population autochtone se rassemble dans des habitations de pierre. Les technologies semblent absentes et les coutumes restes tribales.",
-		"texture" : "planet-textures/jupitermap.jpg"
-	},
-
-
-	{
-		"code" : "ARA-3",
-		"name" : "Ara-Choo-III",
-		"size" : 3,
-		"rhodium" : 3,
-		"hazard" : 2,
-		"position" : {"x" : 5, "y" : 12, "z" : 9},
-		"description" : "Une boule bleu parsemé de petite taches vertes. Les îles sont couverte de mousse et très humide, comme dans un climat de jungle. Il pleut la majorité du temps. ",
-		"texture" : "planet-textures/Planet_New_Aruba_5128.jpg"
-	},
-
-
-	{
-		"code" : "LIB-11",
-		"name" : "Thémis",
-		"size" : 5,
-		"rhodium" : 1,
-		"hazard" : 3,
-		"position" : {"x" : 8, "y" : 8, "z" : 8},
-		"description" : "La planète est couverte à 80% par des bâtiments construit par la race primaire. Le Rhodium est miné et sert d’énergie au habitant. Ceux-ci on tenté de prendre contacte avec la sonde. L’échange de technologie pourrait être profitable à nos deux civilisations.",
-		"texture" : "planet-textures/planet_Trask1200.jpg"
-	},
-
-
-];
+planets =[  
+   {  
+      "id":1,
+      "code":"HC-1389",
+      "name":"Rh-Paradize",
+      "size":5,
+      "rhodium":5,
+      "hazard":4,
+      "position":{  
+         "x":"0",
+         "y":"0",
+         "z":"0"
+      },
+      "description":"Cette plan\u00e8te affiche un caract\u00e8re apocalyptique, des rochers tranchants et des ruisseaux de m\u00e9taux en fusion parsemant sa surface. L\u2019eau est extr\u00eamement rare, et l\u2019aire pratiquement irrespirable. Mais, son potentiel minier est exceptionnel, la quantit\u00e9 de rhodium disponible fait r\u00eaver les corporations.",
+      "texture":"planet-textures\/plutomap1k.jpg",
+      "bump":"planet-bumps\/mars_normal.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-07 09:44:26",
+      "date_updated":"2017-04-24 20:27:53"
+   },
+   {  
+      "id":4,
+      "code":"ZiZ-578",
+      "name":"Ara 425",
+      "size":3,
+      "rhodium":2,
+      "hazard":1,
+      "position":{  
+         "x":"578",
+         "y":"-232",
+         "z":"-425"
+      },
+      "description":"Une boule bleue parsem\u00e9 de petites taches vertes. Les \u00eeles sont couverte de mousse et tr\u00e8s humide. Le climat varie de subtropical \u00e0 tropical \u00e0 l'\u00e9quateur. Les pr\u00e9cipitations fr\u00e9quentes et le couvert nuageux presque opaque rendent la d\u00e9tection de forme de vie impossible \u00e0 valider",
+      "texture":"planet-textures\/Terrestrial3.jpg",
+      "bump":"planet-bumps\/bump.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-07 09:57:21",
+      "date_updated":"2017-04-24 19:18:51"
+   },
+   {  
+      "id":540,
+      "code":"PP-487",
+      "name":"Hercules 1467",
+      "size":3,
+      "rhodium":1,
+      "hazard":3,
+      "position":{  
+         "x":"-487",
+         "y":"791",
+         "z":"1467"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"planet-textures\/Terrestrial3.jpg",
+      "bump":"planet-bumps\/bump.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-25 21:06:51",
+      "date_updated":"2017-04-25 21:06:51"
+   },
+   {  
+      "id":541,
+      "code":"DwY-441",
+      "name":"Hercules 964",
+      "size":5,
+      "rhodium":3,
+      "hazard":4,
+      "position":{  
+         "x":"-441",
+         "y":"904",
+         "z":"964"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"planet-textures\/Gaseous2.jpg",
+      "bump":"planet-bumps\/jupiter_normal.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-25 21:07:18",
+      "date_updated":"2017-04-25 21:07:18"
+   },
+   {  
+      "id":6,
+      "code":"OP-767",
+      "name":"Apus 509",
+      "size":5,
+      "rhodium":3,
+      "hazard":4,
+      "position":{  
+         "x":"767",
+         "y":"-391",
+         "z":"509"
+      },
+      "description":"Plan\u00e8te plut\u00f4t d\u00e9sertique parsem\u00e9 de certain point d\u2019eau o\u00f9 la population autochtone se rassemble dans des habitations de pierre. Les technologies semblent absentes et les coutumes restent tribales. \r\nLa forte gravit\u00e9 de la plan\u00e8te et sa proximit\u00e9 avec son soleil laisse pr\u00e9voir que la vie sur cette plan\u00e8te est tr\u00e8s rude et que l'environnement \u00e0 d\u00fb s'y adapter. ",
+      "texture":"planet-textures\/planet_Tao_Seti_1600.jpg",
+      "bump":"planet-bumps\/pluto_normal.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-24 18:56:22",
+      "date_updated":"2017-04-24 19:13:50"
+   },
+   {  
+      "id":539,
+      "code":"TnH-256",
+      "name":"Hercules 7",
+      "size":3,
+      "rhodium":2,
+      "hazard":2,
+      "position":{  
+         "x":"-256",
+         "y":"417",
+         "z":"-7"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"planet-textures\/Terrestrial2.jpg",
+      "bump":"planet-bumps\/pluto_normal.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-25 21:06:11",
+      "date_updated":"2017-04-25 21:06:11"
+   },
+   {  
+      "id":9,
+      "code":"PP-1384",
+      "name":"Antilla 1309",
+      "size":4,
+      "rhodium":0,
+      "hazard":3,
+      "position":{  
+         "x":"1384",
+         "y":"287",
+         "z":"1309"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"planet-textures\/planet_Dank1200.jpg",
+      "bump":"planet-bumps\/bump.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-24 19:02:53",
+      "date_updated":"2017-04-25 09:59:34"
+   },
+   {  
+      "id":10,
+      "code":"OP-1305",
+      "name":"Antilla 492",
+      "size":2,
+      "rhodium":1,
+      "hazard":4,
+      "position":{  
+         "x":"1305",
+         "y":"354",
+         "z":"492"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"planet-textures\/planet_Dank1200.jpg",
+      "bump":"planet-bumps\/pluto_normal.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-24 19:05:52",
+      "date_updated":"2017-04-24 19:11:18"
+   },
+   {  
+      "id":7,
+      "code":"PP-1500",
+      "name":"Antilla 1260",
+      "size":4,
+      "rhodium":3,
+      "hazard":2,
+      "position":{  
+         "x":"1500",
+         "y":"251",
+         "z":"1260"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"planet-textures\/planet_Tao_Seti_1600.jpg",
+      "bump":"planet-bumps\/lalt_normal.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-24 18:57:28",
+      "date_updated":"2017-04-25 15:06:09"
+   },
+   {  
+      "id":8,
+      "code":"HC-1437",
+      "name":"Antilla 1253",
+      "size":3,
+      "rhodium":2,
+      "hazard":2,
+      "position":{  
+         "x":"1437",
+         "y":"247",
+         "z":"-1253"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"planet-textures\/planet_serendip_1600.jpg",
+      "bump":"planet-bumps\/moon_normal.jpg",
+      "color":"#ffffff",
+      "date_created":"2017-04-24 18:58:49",
+      "date_updated":"2017-04-25 15:06:04"
+   },
+   {  
+      "id":11,
+      "code":"DwY-394",
+      "name":"Aquarius 964",
+      "size":4,
+      "rhodium":1,
+      "hazard":2,
+      "position":{  
+         "x":"-394",
+         "y":"-1146",
+         "z":"964"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"",
+      "bump":"",
+      "color":"#ffffff",
+      "date_created":"2017-04-24 19:12:23",
+      "date_updated":"2017-04-25 21:07:11"
+   },
+   {  
+      "id":12,
+      "code":"JE-168",
+      "name":"Aquarius 245",
+      "size":3,
+      "rhodium":1,
+      "hazard":2,
+      "position":{  
+         "x":"-168",
+         "y":"-1404",
+         "z":"245"
+      },
+      "description":"Analyse des informations en cours...",
+      "texture":"",
+      "bump":"",
+      "color":"#ffffff",
+      "date_created":"2017-04-24 19:13:54",
+      "date_updated":"2017-04-24 19:13:54"
+   }
+]
